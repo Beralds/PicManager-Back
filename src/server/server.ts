d7865bbb84b8599a9515a9c11100e20d112b8864
Wender Lemes <wender.lemes@dtidigital.com.br>
2025-01-25 13:06:32 -0300

    Solving CORS errors

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 import { router } from './routes';
 
 const server = express();
+server.use(cors({
+  origin: '*'
+}));
+
 server.use(router);
 
 export { server };
